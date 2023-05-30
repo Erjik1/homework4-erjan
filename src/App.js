5d683b4f024acc92057fc97a1f0ce8aa5348dfe1
Erjik1 <120021425+Erjik1@users.noreply.github.com>
2023-05-30 13:57:28 +0600

    hw4-erjan

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const RegistrationForm = () => {
+    const [fullName, setFullName] = useState('')
+    const [phoneNumber, setPhoneNumber] = useState('')
+    const [email, setEmail] = useState('')
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target
+
+        if (name === 'fullName') {
+            setFullName(value)
+        } else if (name === 'phoneNumber') {
+            setPhoneNumber(value)
+        } else if (name === 'email') {
+            setEmail(value)
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (fullName.trim() === '' || phoneNumber.trim() === '' || email.trim() === '') {
+            alert('ПОЖАЛУЙСТА ЗАПОЛНИТЕ ФОРМУ!!!')
+            return
+        }
+
+        alert(`Full Name: ${fullName}\nPhone Number: ${phoneNumber}\nEmail: ${email}`)
+
+        setFullName('')
+        setPhoneNumber('')
+        setEmail('')
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1>ФОРМА РЕГИСТРАЦИИ</h1>
+            <div>
+                <label htmlFor="fullName">ФИО:</label>
+                <input
+                    type="text"
+                    id="fullName"
+                    name="fullName"
+                    value={fullName}
+                    onChange={handleInputChange}
+                />
+            </div>
+            <div>
+                <label htmlFor="phoneNumber">НОМЕР:</label>
+                <input
+                    type="text"
+                    id="phoneNumber"
+                    name="phoneNumber"
+                    value={phoneNumber}
+                    onChange={handleInputChange}
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={email}
+                    onChange={handleInputChange}
+                />
+            </div>
+            <button type="submit">НАЖАТЬ</button>
+        </form>
+    )
 }
 
-export default App;
+export default RegistrationForm;
